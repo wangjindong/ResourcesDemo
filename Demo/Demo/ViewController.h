b557a4931e4747413d8ce64a46a588bd173e7904
wangjindong <419591321@qq.com>
2018-03-15 10:16:48 +0800

    init
    init

---
+++ Demo/Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by JD on 2018/3/15.
+//  Copyright © 2018年 JD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Demo/Demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Demo
+//
+//  Created by JD on 2018/3/15.
+//  Copyright © 2018年 JD. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
